885ac10f959d79281d6000661d17439bddd0733a
Orcus-IRL <vkarthick986@gmail.com>
2024-06-01 06:38:33 +0530

    added robot

---
+++ robot/Hurdle 3.py
@@ -0,0 +1,25 @@
+def turn_right():
+       turn_left()
+       turn_left()
+       turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+    
+while not at_goal():
+    if wall_in_front:
+        jump()
+    else: 
+        move()
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ robot/robot_test (1).py
@@ -0,0 +1,15 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
